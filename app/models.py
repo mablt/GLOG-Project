62fe361d5ce206df90e101b2f03e00dfdb1c59c2
mablt <mathieu.bolteau1@gmail.com>
2020-11-10 18:57:35 +0100

    Add frontend test files

--- app/models.py
+++ app/models.py
@@ -1,7 +1,46 @@
 import requests
 import json
 
+########################### LIKE SERVER2 BEFORE ###########################
 
+   
+# @app.route('/result', methods = ['POST', 'GET'])
+def getResponse(name):
+   response = requests.get("https://swissmodel.expasy.org/repository/uniprot/%s.pdb" %name)
+    # Add ?provider=swissmodel after json to get the SwissModel data
+    # https://swissmodel.expasy.org/repository/uniprot/P07900.pdb?sort=seqsim to get the best 
+    # homology model or experimental structure in PDB format sorted by sequence similarity
+   # print(response.text)
+   #  exit()
+   # file = open(response, 'r')
+   return response.text
+
+# @app.route('/result', methods = ['POST', 'GET'])
+def jsonPrint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=2)
+    return text
+
+# @app.route('/result', methods = ['POST', 'GET'])
+def print_pdb(protein):
+
+   # Get the JSON
+   pdbBrutData = getResponse(protein)
+   return pdbBrutData
+
+   # # Get some useful informations from the BRUT json file
+   # print("sequence :", jsonBrutData['result']['sequence'])               # Get the sequence
+   # print('sequence length :', jsonBrutData['result']['sequence_length'])        # Get the sequence length
+   # print("pdb file link :", jsonBrutData['result']['structures'][0]['coordinates'])       # Get the url of the pdb file
+   # print("modelling model :", jsonBrutData['result']['structures'][0]['method'])       # Get the modelling method
+   # print("protein number :", jsonBrutData['result']['uniprot_entries'][0]['ac'])       # Get the qualifier id
+   # print("protein id :", jsonBrutData['result']['uniprot_entries'][0]['id'])       # Get the id of the protein
+   # return pdbBrutData
+
+
+########################### LIKE SERVER1 BEFORE ###########################
+
+
+"""
 def getResponse(name):
     response = requests.get("https://swissmodel.expasy.org/repository/uniprot/%s.json" %name)
     # Add ?provider=swissmodel after json to get the SwissModel data
@@ -26,3 +65,5 @@ def print_pdb(protein):
    # print("protein number :", jsonBrutData['result']['uniprot_entries'][0]['ac'])       # Get the qualifier id
    # print("protein id :", jsonBrutData['result']['uniprot_entries'][0]['id'])       # Get the id of the protein
    return pdbBrutData
+
+"""
--- app/views.py
+++ app/views.py
@@ -3,6 +3,25 @@ from flask import render_template, request
 from .models import print_pdb
 
 
+
+########################### LIKE SERVER2 BEFORE ###########################
+
+@app.route('/', methods = ['POST', 'GET'])
+def index():
+   return render_template("index.html")
+
+@app.route('/result', methods = ['POST', 'GET'])
+def get_pdb():
+   if request.method == "POST":
+      # protein = print_pdb(request.form["nm"])
+      protein = getResponse(request.form["nm"])
+      return render_template("printmolecule.html", protein = protein)
+ 
+
+########################### LIKE SERVER1 BEFORE ###########################
+
+
+"""
 @app.route('/', methods = ['POST', 'GET'])
 def index():
    return render_template("hello.html")
@@ -12,3 +31,5 @@ def get_pdb():
    if request.method == "POST":
       protein = print_pdb(request.form["nm"])
       return render_template("result.html", protein = protein)
+
+"""
