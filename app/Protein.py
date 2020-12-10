1074086f89c54b25b05e11fc7a59b552a34c8a9d
mablt <mathieu.bolteau1@gmail.com>
2020-12-10 16:50:39 +0100

    Add comments

--- app/Protein.py
+++ app/Protein.py
@@ -3,17 +3,15 @@
 
 """
 Protein.py: Protein class of the GLOG Project.
-
-DESCRITPION #TODO
 """
 
 __author__ = "ALVES Marine, BOLTEAU Mathieu, CARRIAT Mélanie, CORNIER Alexandre, DRANCÉ Martin, JELIN Rémy and NEUHAUS Abdelghani"
-__copyright__ = "Copyright ?? December 2020"  # TODO
+__copyright__ = "Copyright (C) 2020, ALVES M., BOLTEAU M., CARRIAT M., CORNIER A., DRANCÉ M., JELIN R. and NEUHAUS A."
+__license__ = "GNU GPL3"
 __version__ = "1.0.0"
 
 # Libraries imports
 import requests
-import json
 from xml.etree import ElementTree as ETree
 from RamachanDraw import phi_psi, plot
 import tempfile
@@ -28,8 +26,48 @@ from .PDBHandler import PDBHandler
 
 
 class Protein():
+    """
+    Class to represent a protein.
+
+    ...
+
+    Attributes
+    ----------
+    id                (str)       : Id of the protein
+    name              (str)       : Name of the protein
+    gene                (str)       : Name of the gene which the porotein come
+    organism           (str)       : Name of the specie for the protein
+    m_seq               (str)       : Sequence of the protein
+    length            (int)       : Length of the protein
+    xml                 (Element)   : XML Element of the XML file parsed
+    pdb_content         (str)       : Content of the PDB file as a string
+    blast_ids           (list)      : List of the ids of the three best hits of the Blast
+    prediction_figure   (str)       : String which represent the base64 signature of the 2D prediction figure
+    ramachandran_figure (str)       : String which represent the base64 signature of the ramachandran figure
+
+
+    Methods
+    -------
+    import_pdb_file_content():
+        Import the data (as string) from the PDB file of the protein and put it to 'pdb_content' variable
+    make_2D_prediction():
+        Make the 2D prediction and put the matplotlib figure object in 'prediction_figure' variable
+    import_xml_data_from_uniprot():
+        Import the data (as string) from the XML file of the protein parsed from a request
+    make_ramachandran():
+        Make the ramachandran for the protein
+    make_blast():
+        Make Blast for the protein and add the ids of the three first hits to 'self.blast_ids' variable
+    transform_2D_prediction_figure(prediction_figure):
+        Transform the 2D prediction figure into string of bytes
+    transform_ramachandran_figure(ramachandran_figure)
+        Transform the ramachandran figure to string of bytes
+    get_attributes():
+        Get all protein attributes without 'self.blast_ids' and 'self.ramachandran_figure'
+    """
+
     def __init__(self, id):
-        self.m_id = id
+        self.id = id
         self.import_pdb_file_content()
         self.make_2D_prediction()
         self.blast_ids = self.ramachandran_figure = None
@@ -37,28 +75,31 @@ class Protein():
 
     def import_pdb_file_content(self):
         """
-        Import the data (as string) from the pdb file of the protein and put it to 'pdb_content' variable
+        Import the data (as string) from the PDB file of the protein and put it to 'pdb_content' variable
         """
-        with open("./app/pdb/"+self.m_id+".pdb", 'r') as pdb_file_object:
+        with open("./app/pdb/"+self.id+".pdb", 'r') as pdb_file_object:
             self.pdb_content = pdb_file_object.read()
 
     def make_2D_prediction(self):
         """
         Make the 2D prediction and put the matplotlib figure object in 'prediction_figure' variable
         """
-        load = PDBHandler(self.m_id, "./app/pdb/")
+        load = PDBHandler(self.id, "./app/pdb/")
         data, length = load.data_creation()
         model = ProteinPlotter(data, length)
         figure = model.draw_2D_protein()
-        encoded_prediction_figure = self.encode_2D_prediction_figure(figure)
+        encoded_prediction_figure = self.transform_2D_prediction_figure(figure)
         self.prediction_figure = encoded_prediction_figure
 
     def import_xml_data_from_uniprot(self):
+        """
+        Import the data (as string) from the XML file of the protein parsed from a request
+        """
         path = "{http://uniprot.org/uniprot}"
         response = requests.get(
-            "https://www.uniprot.org/uniprot/{}.xml".format(self.m_id))
+            "https://www.uniprot.org/uniprot/{}.xml".format(self.id))
         self.xml = ETree.fromstring(response.content)
-        self.m_name = self.xml.find(
+        self.name = self.xml.find(
             "{}entry/{}protein/{}recommendedName/{}fullName".format(path, path, path, path)).text
         self.gene = self.xml.find(
             "{}entry/{}gene/{}name".format(path, path, path))
@@ -66,21 +107,29 @@ class Protein():
             self.gene = "N.A."
         else:
             self.gene = self.gene.text
-        self.m_species = self.xml.find(
+        self.organism = self.xml.find(
             "{}entry/{}organism/{}name".format(path, path, path)).text
         self.m_seq = self.xml.find(
             "{}entry/{}sequence".format(path, path)).text
-        self.m_length = self.xml.find(
+        self.length = self.xml.find(
             "{}entry/{}sequence".format(path, path)).attrib['length']
 
     def make_ramachandran(self):
-        path = "./app/pdb/"+self.m_id+".pdb"
+        """
+        Make the ramachandran for the protein
+        """
+        path = "./app/pdb/"+self.id+".pdb"
         data = phi_psi(path)  # ramachadran data
+        # Create a tempfile to save the figure file
         figure = tempfile.TemporaryFile()
         plot(path, out=figure)
-        self.ramachandran_figure = self.encode_ramachandran_figure(figure)
+        # Put the byte-string of the figure in the 'self.ramachandran_figure' variable
+        self.ramachandran_figure = self.transform_ramachandran_figure(figure)
 
     def make_blast(self):
+        """
+        Make Blast for the protein and add the ids of the three first hits to 'self.blast_ids' variable
+        """
         query = Query(self.m_seq, query_type="sequence",
                       return_type="polymer_entity")
         search = query.search()
@@ -89,46 +138,72 @@ class Protein():
             long_id = search["result_set"][i]["identifier"]
             id = long_id.split('_')[0]   # Remove '_1' at the end of the ID
             self.blast_ids.append(id)
-            
-    def encode_2D_prediction_figure(self, prediction_figure):
+
+    def transform_2D_prediction_figure(self, prediction_figure):
+        """
+        Transform the 2D prediction figure into string of bytes
+
+        Args:
+            prediction_figure (Figure): Matplotlib figure of the 2D prediction
+
+        Returns:
+            str: 2D prediction figure in bytes-string
+        """
+        # Transform the Figure object to bytes object
         img = io.BytesIO()
         prediction_figure.savefig(img, format='png', bbox_inches='tight')
         img.seek(0)
+        # Encode to bytes object the figure bytes value
         encoded = base64.b64encode(img.getvalue())
+        # Return the bytes figure as string
         return encoded.decode('utf-8')
 
-    def encode_ramachandran_figure(self, ramachandran_figure):
+    def transform_ramachandran_figure(self, ramachandran_figure):
+        """
+        Transform the ramachandran figure to string of bytes
+
+        Args:
+            ramachandran_figure (TemporaryFile): Temporary file which contains the ramachandran figure
+
+        Returns:
+            str: Ramachandran figure in bytes-string
+        """
         ramachandran_figure.seek(0)
         encoded = base64.b64encode(ramachandran_figure.read())
         return encoded.decode('utf-8')
 
-    def get_pdb(self):
-        return self.pdb_content
+    def get_attributes(self):
+        """
+        Get all protein attributes without 'self.blast_ids' and 'self.ramachandran_figure'
 
-    def get_id(self):
-        return self.m_id
-
-    def get_name(self):
-        return self.m_name
-
-    def get_length(self):
-        return self.m_length
-
-    def get_seq(self):
-        return self.m_seq
-
-    def get_species(self):
-        return self.m_species
-
-    def get_gene(self):
-        return self.gene
-
-    def get_2D_prediction_figure(self):
-        return self.prediction_figure
+        Returns:
+            dict: Dictionnary which contains the attibutes of the protein
+        """
+        attributes = {
+            "id": self.id,
+            "name": self.name,
+            "organism": self.organism,
+            "length": self.length,
+            "gene": self.gene,
+            "pdb": self.pdb_content,
+            "2D_prediction":self.prediction_figure,
+        }
+        return attributes
 
     def get_blast_ids(self):
+        """
+        Blast ids list getter
+
+        Returns:
+            list: List of the ids of the three better hits
+        """
         return self.blast_ids
 
     def get_ramachandran_figure(self):
-        return self.ramachandran_figure
+        """
+        Ramachandran figure getter
 
+        Returns:
+            str: Ramachandran figure as bytes-string
+        """
+        return self.ramachandran_figure
--- app/ProteinRepository.py
+++ app/ProteinRepository.py
@@ -3,24 +3,53 @@
 
 """
 Protein.py: ProteinRepository class of the GLOG Project.
-
-DESCRITPION #TODO
 """
 
 __author__ = "ALVES Marine, BOLTEAU Mathieu, CARRIAT Mélanie, CORNIER Alexandre, DRANCÉ Martin, JELIN Rémy and NEUHAUS Abdelghani"
-__copyright__ = "Copyright ?? December 2020" #TODO
-__version__ = "1.0.0" 
+__copyright__ = "Copyright (C) 2020, ALVES M., BOLTEAU M., CARRIAT M., CORNIER A., DRANCÉ M., JELIN R. and NEUHAUS A."
+__license__ = "GNU GPL3"
+__version__ = "1.0.0"
 
 # Local imports
 from .Protein import Protein
 
 class ProteinRepository():
+    """
+    Class to represent a protein repository.
+
+    This class permits to communicate with the frontend. It execute all of the functionnalities 
+    and return all needed informations for the frontend.
+    ...
+
+    Attributes
+    ----------
+    proteins            (dict)      : Dictionnary which contains proteins object. Keys are the id of the protein. Values are Protein objects.
+
+
+    Methods
+    -------
+    add_protein(id):
+        Add a protein object to the repository if is not already present
+    get_2D_prediction(id)
+        Get the 2D prediction figure of the protein with the ID 'id'
+    is_absent(id):
+        Check if the protein with the ID 'id' is absent of the repository
+    get_protein_by_id(id)
+        Return the Protein object with the ID 'id'
+    get_protein_informations_by_id(id):
+        Return protein informations of the protein with the ID 'id'
+    get_blast(id):
+        Get blast data of the protein with the ID 'id' and make it if it does not already done
+    get_ramachandran(id):
+        Get the ramachandran figure of the protein with the ID 'id' and it if it does not already done
+    """
     
     def __init__(self):
         self.proteins = dict()
         
     def add_protein(self, id):
-        """Add a protein object to the repository
+        """
+        Add a protein object to the repository if is not already present
 
         Args:
             id (string): ID of the protein
@@ -30,7 +59,8 @@ class ProteinRepository():
             self.proteins[id] = protein
             
     def get_2D_prediction(self, id):
-        """Get the 2D prediction figure of the protein with the ID 'id'
+        """
+        Get the 2D prediction figure of the protein with the ID 'id'
 
         Args:
             id (string): ID of the protein
@@ -46,7 +76,8 @@ class ProteinRepository():
     
     
     def is_absent(self,id):
-        """Check if the protein with the ID 'id' is absent of the repository
+        """
+        Check if the protein with the ID 'id' is absent of the repository
 
         Args:
             id (string): ID of the protein
@@ -60,7 +91,8 @@ class ProteinRepository():
         
         
     def get_protein_by_id(self, id):
-        """Return the Protein object with the ID is 'id'
+        """
+        Return the Protein object with the ID 'id'
 
         Args:
             id (string): ID of the protein
@@ -73,26 +105,45 @@ class ProteinRepository():
         return self.proteins.get(id)
     
     def get_protein_informations_by_id(self, id):
+        """
+        Return protein informations of the protein with the ID 'id'
+
+        Args:
+            id (str): ID of the protein
+
+        Returns:
+            dict: Protein informations wanted
+        """
         protein = self.get_protein_by_id(id)
-        data = {
-            "id": protein.get_id(),
-            "name": protein.get_name(),
-            "species": protein.get_species(),
-            "length": protein.get_length(),
-            "gene": protein.get_gene(),
-            "pdb": protein.get_pdb(),
-            "2D_prediction": protein.get_2D_prediction_figure(),
-        }
+        data = protein.get_attributes()
         return data
     
-    def make_blast(self, id):
+    def get_blast(self, id):
+        """
+        Get blast data of the protein with the ID 'id' and make it if it does not already done
+
+        Args:
+            id (str): ID of the protein
+
+        Returns:
+            list: List of the ids of the three better hits of the Blast
+        """
         protein = self.proteins.get(id)
         if protein.get_blast_ids() is None:
             protein.make_blast()
         return protein.get_blast_ids()
         
     
-    def make_ramachandran(self, id):
+    def get_ramachandran(self, id):
+        """
+        Get the ramachandran figure of the protein with the ID 'id' and it if it does not already done
+
+        Args:
+            id (str): ID of the protein
+
+        Returns:
+            str: Ramachandran figure as bytes-string
+        """
         protein = self.proteins.get(id)
         if protein.get_ramachandran_figure() is None:
             protein.make_ramachandran()
